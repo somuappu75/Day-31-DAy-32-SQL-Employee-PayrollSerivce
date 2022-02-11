47b9a3f392c6c877ff538fb622acaf6c90346679
Somanath Havinal <somusp75@gmail.com>
2022-02-11 17:25:55 +0530

    uc-7 aggregate functions
    aggregate functions

--- PayRoll.sql
+++ PayRoll.sql
@@ -17,19 +17,26 @@ Insert into employee_payroll(name,salary,startDate) values
 ('chetan',11000,'2021-05-13'),
 ('gouri',35000,'2021-08-19');
 
-
 --uc-4 retrieving all data
 select *from employee_payroll;
 ---uc-5 retrieving data from cast() anf Getdate()
 select salary from employee_payroll where name='suresh';
 select salary from employee_payroll where startDate BETWEEN Cast('2021-08-19' as Date) and GetDate();
 
+--uc-6 add column gender and upadate values
+Alter table employee_payroll
+add Gender char(1);
 
+Update employee_payroll 
+set Gender='M'
+where name='suresh';
+Update employee_payroll 
+set Gender='F'
+where name='chetan' or name='somu'or name='gouri';
 
-
-
-
-
-
-
-
+--uc-7 Aggregate function anfd Gruop by gender
+select Sum(salary) as "TotalSalary",Gender from employee_payroll group by Gender;
+select Avg(salary) as "AverageSalary",Gender from employee_payroll group by Gender;
+select Min(salary) as "MinimumSalary",Gender from employee_payroll group by Gender;
+select Max(salary) as "MaximumSalary",Gender from employee_payroll group by Gender;
+select count(salary) as "CountSalary",Gender from employee_payroll group by Gender;
