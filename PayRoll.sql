704de82e1c6e1f4a6b0177221ea59afa563e98f1
Somanath Havinal <somusp75@gmail.com>
2022-02-11 16:55:33 +0530

    retrieving all data
    retrieving data

--- PayRoll.sql
+++ PayRoll.sql
@@ -8,3 +8,14 @@ name varchar(200) not null,
 salary float,
 startDate date
 )
+
+
+--uc -3 inserting values to table
+Insert into employee_payroll(name,salary,startDate) values
+('somu',22000,'2022-12-12'),
+('suresh',25000,'2022-08-18'),
+('chetan',11000,'2021-05-13'),
+('gouri',35000,'2021-08-19');
+
+--uc-4 retrieving all data
+select *from employee_payroll;
