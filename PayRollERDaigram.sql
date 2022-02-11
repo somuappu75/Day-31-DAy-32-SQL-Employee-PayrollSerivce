4492cd116acad3434ebd3582e01000adf818bd09
Somanath Havinal <somusp75@gmail.com>
2022-02-11 23:10:35 +0530

    uc-9 extended employee payroll table
    adding columns

--- PayRollERDaigram.sql
+++ PayRollERDaigram.sql
@@ -1,50 +1,3 @@
-create database PayRoll_Service;
-
---uc2 creating table
-create Table employee_payroll
-(
-id int identity(1,1) primary key,
-name varchar(200) not null,
-salary float,
-startDate date
-)
-
-
---uc -3 inserting values to table
-Insert into employee_payroll(name,salary,startDate) values
-('somu',22000,'2022-12-12'),
-('suresh',25000,'2022-08-18'),
-('chetan',11000,'2021-05-13'),
-('gouri',35000,'2021-08-19');
-
---uc-4 retrieving all data
-select *from employee_payroll;
----uc-5 retrieving data from cast() anf Getdate()
-select salary from employee_payroll where name='suresh';
-select salary from employee_payroll where startDate BETWEEN Cast('2021-08-19' as Date) and GetDate();
-
-
-
-
---uc-6 add column gender and upadate values
-Alter table employee_payroll
-add Gender char(1);
-
-Update employee_payroll 
-set Gender='M'
-where name='suresh';
-Update employee_payroll 
-set Gender='F'
-where name='chetan' or name='somu'or name='gouri';
-
-
---uc-7 Aggregate function anfd Gruop by gender
-select Sum(salary) as "TotalSalary",Gender from employee_payroll group by Gender;
-select Avg(salary) as "AverageSalary",Gender from employee_payroll group by Gender;
-select Min(salary) as "MinimumSalary",Gender from employee_payroll group by Gender;
-select Max(salary) as "MaximumSalary",Gender from employee_payroll group by Gender;
-select count(salary) as "CountSalary",Gender from employee_payroll group by Gender;
-
 ------- UC 8: Add column department,PhoneNumber and Address -------
 Alter table employee_payroll
 add EmployeePhoneNumber BigInt,EmployeeDepartment varchar(200) not null default 'Publish',Address varchar(200) default 'Not Provided';
@@ -95,10 +48,3 @@ set NetPay=(basicPay - Deduction)
 update employee_payroll
 set TaxablePay=0,IncomeTax=0
 select * from employee_payroll;
-
-
-
-
-
-
-
